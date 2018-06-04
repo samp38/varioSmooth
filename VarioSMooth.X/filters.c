90d3731a6eb9f732853bf9e7663d89859c621c84
samp38 <samuel.heidmann@gmail.com>
2018-06-04 18:20:04 +0200

    Doc
    Doc

--- VarioSMooth.X/filters.c
+++ VarioSMooth.X/filters.c
@@ -9,12 +9,12 @@
 
 
 // feedforward coefficients
-#define B_0     16
-#define B_1  	32
-#define B_2  	16
+#define B_0_s    4 // shift 4 = *16
+#define B_1_s  	 5 // shift 5 = *32
+#define B_2_s  	 4 // shift 4 = *16
 // feedback coefficients,
-#define A_0 	2048
-#define A_1  	-3553
+#define A_0_sr 	11 // shift 11 right = div by 2048
+#define A_1  	3553
 #define A_2  	1569
 
 #define INTEGER_FACTOR 11       // filter Value will be multiplied by 2ÎNTEGER_FACTOR for precision
@@ -25,16 +25,16 @@ s64 y_1 = 0LL;
 s64 y_2 = 0LL;
 
 s32 filterPressure(s32 x) {
-    s64 xBIG = x << INTEGER_FACTOR;
-    s64 y = ((s64)B_0*(s64)xBIG) + ((s64)B_1*x_1) + ((s64)B_2*x_2) - ((s64)A_1*y_1) - ((s64)A_2*y_2);
+    s64 xBIG = (s64)(x) << INTEGER_FACTOR;
+    s64 y = (xBIG<<B_0_s) + (x_1<<B_1_s) + (x_2<<B_2_s) + (((s64)A_1*y_1) - ((s64)A_2*y_2));
 
     x_2 = x_1;
     x_1 = xBIG;
 
     y_2 = y_1;
-    y_1 = y/A_0;
+    y_1 = y>>A_0_sr;
 
-    return ((s32)y_1) >> INTEGER_FACTOR;
+    return (s32)(y_1 >> INTEGER_FACTOR);
 }
 
 
